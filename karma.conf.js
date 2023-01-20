cee4deb413023c0084b2c20da87cefe9a69bc3fa
Solomon Liberta <solomon.liberta@postclick.com>
2023-01-19 23:23:14 -0600

    fix: removing company related info

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/beta-nxt-atm'),
+      dir: require('path').join(__dirname, './coverage/atm-poc'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,16 +22,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  /*it(`should have as title 'BetaNxt-ATM'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('BetaNxt-ATM');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BetaNxt-ATM app is running!');
-  });*/
 });
